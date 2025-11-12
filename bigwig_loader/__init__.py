7635692799308d06f0cf8367751b61f6dcd04742
Joren Retel <joren.retel@pfizer.com>
2025-11-12 15:36:53 +0100

    Log a warning for the user about changed dimensionality

--- bigwig_loader/__init__.py
+++ bigwig_loader/__init__.py
@@ -1,5 +1,4 @@
-__author__ = ["Joren Retel"]
-__email__ = ["joren.retel@pfizer.com"]
+import logging
 
 try:
     from bigwig_loader._version import version as __version__
@@ -10,5 +9,22 @@ except ImportError:
     __version__ = "not-installed"
 
 from bigwig_loader.settings import Settings as _Settings
+
+__author__ = ["Joren Retel"]
+__email__ = ["joren.retel@pfizer.com"]
+
+logger = logging.getLogger("bigwig_loader")
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+logger.setLevel(logging.WARNING)
+
+
+logger.warning(
+    "Since bigwig-loader v0.3.0 the order of the output "
+    "has changed to (batch x sequence x tracks)"
+)
 
 config = _Settings()
